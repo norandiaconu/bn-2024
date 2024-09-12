9428ed71788be94eab5ed944a56379b294800ad2
Noran Diaconu <norandiaconu@gmail.com>
2024-09-11 20:49:40 -0400

    Removed bullets and added click events to links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,8 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
     title = 'bn-2024';
-    vh = 0;
-    onHome = false;
-
-    constructor() {
-        this.vh = window.innerHeight;
-    }
 
     public scroll(el: HTMLElement): void {
         el.scrollIntoView({ behavior: 'smooth' });
-    }
-
-    @HostListener('document:scroll', ['$event'])
-    public onViewportScroll() {
-        if (window.scrollY < 1000) {
-            this.onHome = true;
-        }
     }
 }
