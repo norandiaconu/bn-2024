b072cbc6d316847b8836d694860e2d4a4d92f3e4
Noran Diaconu <norandiaconu@gmail.com>
2024-09-13 22:31:36 -0400

    Added countdown, page info, and fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -7,6 +7,22 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
     title = 'bn-2024';
+    days = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    constructor() {
+        const countDownDate = new Date('Nov 16, 2024 15:00:00').getTime();
+        setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countDownDate - now;
+            this.days = Math.floor(distance / 86400000);
+            this.hours = Math.floor((distance % 86400000) / 3600000);
+            this.minutes = Math.floor((distance % 3600000) / 60000);
+            this.seconds = Math.floor((distance % 60000) / 1000);
+        }, 1000);
+    }
 
     public scroll(el: HTMLElement): void {
         el.scrollIntoView({ behavior: 'smooth' });
