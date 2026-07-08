126eb7db14ae7205066deee1e1949ee0f1177e43
Noran Diaconu <norandiaconu@gmail.com>
2026-07-08 19:24:46 -0400

    Updated linting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { describe, it, beforeEach, expect } from 'vitest';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
