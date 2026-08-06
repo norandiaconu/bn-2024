9a8c0c523de2a232c5f949f3403c672146182ebc
Noran Diaconu <norandiaconu@gmail.com>
2026-08-05 23:32:21 -0400

    Changed to zoneless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -7,20 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     protected title = 'bn-2024';
-    protected days = 0;
-    protected hours = 0;
-    protected minutes = 0;
-    protected seconds = 0;
+    protected days = signal<number>(0);
+    protected hours = signal<number>(0);
+    protected minutes = signal<number>(0);
+    protected seconds = signal<number>(0);
 
     constructor() {
         const countDownDate = new Date('Nov 16, 2024 15:00:00').getTime();
         setInterval(() => {
             const now = new Date().getTime();
             const distance = countDownDate - now;
-            this.days = Math.floor(distance / 86400000);
-            this.hours = Math.floor((distance % 86400000) / 3600000);
-            this.minutes = Math.floor((distance % 3600000) / 60000);
-            this.seconds = Math.floor((distance % 60000) / 1000);
+            this.days.set(Math.floor(distance / 86400000));
+            this.hours.set(Math.floor((distance % 86400000) / 3600000));
+            this.minutes.set(Math.floor((distance % 3600000) / 60000));
+            this.seconds.set(Math.floor((distance % 60000) / 1000));
         }, 1000);
     }
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideZoneChangeDetection(),importProvidersFrom(BrowserModule)],
+    providers: [importProvidersFrom(BrowserModule)]
 }).catch((err) => console.error(err));
